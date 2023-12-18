8a71517861fae6814c2168b980aeb59112552a5d
am <arlindmaliqi28@gmail.com>
2023-12-18 18:10:29 +0100

    hotfix

--- front/src/components/custom/AuthOutro.jsx
+++ front/src/components/custom/AuthOutro.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Button from "../common/Button";
+import useToggle from "../../hooks/useToggle";
 
-export default function AuthOutro({ effect, essentials, auto, type }) {
+export default function AuthOutro({ essentials, type }) {
+  const { auto, effect } = useToggle();
+
   return (
     <div className="py-4 flex-shrink-0 px-0">
       <div
@@ -15,7 +18,7 @@ export default function AuthOutro({ effect, essentials, auto, type }) {
             onClick={auto}
             className={`rounded-full p-1 ring-4 flex justify-center items-center ${
               effect
-                ? "bg-[#0008C7] active:ring-blue-100 ring-transparent"
+                ? "bg-[#0008C7] hover:bg-blue-800 active:ring-blue-100 ring-transparent"
                 : "bg-zinc-200 ring-transparent"
             } ${type === "signup" ? "w-[35px] h-7" : "w-7 h-7"}`}
           >
@@ -37,14 +40,14 @@ export default function AuthOutro({ effect, essentials, auto, type }) {
           )}
         </div>
         <div className="w-[60vw]">
-          <Button
+          <button
             type="submit"
             disabled={effect === false}
-            style={`
+            className={`
       h-full border py-2.5 text-2xl flex items-center font-bold w-full h-full rounded-full px-12 transition-all duration-300 shadow-md
       ${
-        effect
-          ? "bg-[#0008C7] group text-white border-blue-500 justify-around"
+        effect != false
+          ? "bg-[#0008C7] group text-white border-[#0008C7] justify-around hover:bg-blue-800 hover:border-blue-800 active:bg-[#0008C7] active:border-[#0008C7]"
           : "text-zinc-300 border-zinc-50  justify-center hover:bg-zinc-50 hover:cursor-not-allowed"
       }
     `}
@@ -58,7 +61,7 @@ export default function AuthOutro({ effect, essentials, auto, type }) {
               {" "}
               {effect ? essentials.arrow_right : ""}{" "}
             </div>
-          </Button>
+          </button>
         </div>
       </div>
     </div>
