3087ce4b0fb7bf162e595216c4f425d87fdaf07d
am <arlindmaliqi28@gmail.com>
2023-12-22 00:58:47 +0100

    ew

--- front/src/layouts/ManagerLayout.jsx
+++ front/src/layouts/ManagerLayout.jsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { useStateContext } from "../contexts/AuthProvider";
 import { Outlet, useNavigate } from "react-router-dom";
 
 export default function ManagerLayout() {
   const { currentUser, userToken } = useStateContext();
+  const [authenticating, setAuthenticating] = useState(true);
   const navigate = useNavigate();
+
   if (userToken) {
+    setAuthenticating(false);
     return (
-      <div>
+      <div className="w-full h-full">
         <Outlet />
       </div>
     );
   } else {
-    navigate("../home");
+    navigate("../auth");
   }
 }
