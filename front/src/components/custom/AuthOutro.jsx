8483cbe90bc165eb2d3295c03445d0061c6a5930
am <arlindmaliqi28@gmail.com>
2023-12-30 12:52:55 +0100

    hotfix

--- front/src/components/custom/AuthOutro.jsx
+++ front/src/components/custom/AuthOutro.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import Button from "../common/Button";
 import useToggle from "../../hooks/useToggle";
 
-export default function AuthOutro({ essentials, type, effect, auto }) {
+function AuthOutro({ essentials, type, effect, auto }) {
   return (
-    <div className="py-4 flex-shrink-0 px-0">
+    <div className="py-4 flex-shrink-0 px-0 h-full">
       <div
         className={`flex gap-2 flex-col items-center justify-end w-full ${
-          type === "signup" ? "h-[20vh]" : "h-[25vh]"
+          type === "signup" ? "h-full" : "h-full"
         }`}
       >
         <div className="flex flex-row gap-2 w-full">
@@ -42,10 +42,10 @@ export default function AuthOutro({ essentials, type, effect, auto }) {
             type="submit"
             disabled={effect === false}
             className={`
-      h-full border py-2.5 text-2xl flex items-center font-bold w-full h-full rounded-full px-12 transition-all duration-300 shadow-md
+      h-full border py-2.5 text-2xl flex items-center font-bold w-full rounded-full px-12 transition-all duration-300 shadow-md
       ${
         effect != false
-          ? "bg-[#0008C7] group text-white border-[#0008C7] justify-around hover:bg-blue-800 hover:border-blue-800 active:bg-[#0008C7] active:border-[#0008C7]"
+          ? "bg-[#0008C7] group text-white border-[#0008C7] justify-center gap-4 hover:bg-blue-800 hover:border-blue-800 active:bg-[#0008C7] active:border-[#0008C7]"
           : "text-zinc-300 border-zinc-50  justify-center hover:bg-zinc-50 hover:cursor-not-allowed"
       }
     `}
@@ -65,3 +65,5 @@ export default function AuthOutro({ essentials, type, effect, auto }) {
     </div>
   );
 }
+
+export default React.memo(AuthOutro);
