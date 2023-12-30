b9b6cfeb465571180f9093c13a62fcf755a577f7
am <arlindmaliqi28@gmail.com>
2023-12-30 23:46:06 +0100

    signin hotfix

--- front/src/components/forms/Signin.jsx
+++ front/src/components/forms/Signin.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Input from "../common/Input";
 
 export default function Signin({ form, onChange, essentials, isError }) {
